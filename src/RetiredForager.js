8068478261cf6d6adcf0f15f2c44f8762a9e925a
Gurjot Singh <imasingh@ucdavis.edu>
2019-10-28 12:07:19 -0700

    completed beesbeesbees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  Grub.call(this);
+  ForagerBee.call(this); // this is how u inhereit multiple properties
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false
+  this.color = 'grey';
 };
 
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype); //this is how u inhereit methods
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function () {
+  return 'I am too old, let me play cards instead'
+}
+
+RetiredForagerBee.prototype.gamble = function () {
+  this.treasureChest.push('treasure');
+};
